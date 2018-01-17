71d513c86ab50abd098138dc22474e9211fe4937
Бельянинов Евгений Евгеньевич <belyaninov.evgeniy@mtsretail.ru>
2018-01-17 17:59:28 +0300

    Зачатки диздока

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace RogueNeverDie
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameRogue())
                 game.Run();
         }
     }
